92b9271ee1b60b2f63f73fa9e6ef9911269960ee
Fenil-Moradiya <moradiyafenil99@gmail.com>
2025-12-25 18:34:51 +0530

    [ADD] Email tracker

---
+++ email_tracker/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Email Tracker",
+    "summary": "Tracks email",
+    "version": "1.0.0",
+    "description": "Track email and log time of opening and notify the responsible user.",
+    "author": "Leofren Technologies",
+    "website": "https://www.leofren.com",
+    "depends": ["mail"],
+    "data": ["views/res_config_settings_views.xml"],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "licence": "LGPL-3",
+}
