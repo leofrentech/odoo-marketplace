f842ba19d011a58896a7d01315f0777acb6cd181
makpa0603 <makpa.share@gmail.com>
2026-07-27 18:32:15 +0530

    [FIX] Fixed method does not exist error

--- access_management/static/src/chatter/chatter.js
+++ access_management/static/src/chatter/chatter.js
@@ -4,7 +4,6 @@ import { patch } from "@web/core/utils/patch";
 import { Chatter } from "@mail/chatter/web_portal/chatter";
 import { onWillStart } from "@odoo/owl";
 
-
 patch(Chatter.prototype, {
     setup() {
         super.setup(...arguments);
@@ -12,7 +11,27 @@ patch(Chatter.prototype, {
     },
 
     async onWillStart() {
-        const access = await this.orm.call(this.props.threadModel, 'get_chatter_data', [], {})
-        Object.assign(this.state, access)
-    }
-})
+        try {
+            const access = await this.orm.call(
+                this.props.threadModel,
+                "get_chatter_data",
+                [],
+                {}
+            );
+
+            if (access) {
+                Object.assign(this.state, access);
+            }
+        } catch (error) {
+            // Ignore "method does not exist" errors.
+            if (
+                error?.data?.name === "builtins.AttributeError" ||
+                error?.message?.includes("does not exist")
+            ) {
+                return;
+            }
+
+            throw error;
+        }
+    },
+});
