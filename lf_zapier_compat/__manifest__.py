aa4d9944653897a7ea96ba669e5b371777bb143b
calkikhunt <calkikhunt123@gmail.com>
2026-09-09 23:30:45 +0530

    [ADD] lf_zapier_compat

--- lf_zapier_compat/__manifest__.py
+++ lf_zapier_compat/__manifest__.py
@@ -21,7 +21,7 @@ it, rather than erroring - without changing behavior for any normal call.
     'website': 'https://leofren.com',
     'support': 'contact@leofren.com',
     'depends': ['base'],
-    'images': ['static/description/banner.png', 'static/description/icon.png'],
+    'images': ['static/description/banner/lf_zapier_compat_cover_light.gif'],
     'installable': True,
     'application': False,
     'auto_install': False,
